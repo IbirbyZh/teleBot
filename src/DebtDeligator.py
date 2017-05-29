6ef79807b7b900297c3600fd5c740cc335d08c73
Шишкин Евгений Андреевич <040796@gmail.com>
2017-05-29 19:50:42 +0300

    Изменил структуру, локализовал на русский

--- src/DebtDeligator.py
+++ src/DebtDeligator.py
@@ -5,38 +5,27 @@ import random
 class DebtDeligator(object):
     def __init__(self, user_ids):
         self.user_ids = user_ids
-        self.want_to_pay = {user_id: -1 for user_id in user_ids}
         self.data = DataBase(user_ids, 'data.json')
         self.transactions = {}
 
-    def set_amount(self, from_user, how_much):
-        self.want_to_pay[from_user] = how_much
-
-    def is_amount_set(self, from_user):
-        return self.want_to_pay[from_user] > 0
-
-    def add_transaction(self, from_user, to_user):
-        if self.want_to_pay[from_user] == -1:
-            return False
+    def add_transaction(self, from_user, to_user, how_much):
         transaction_hash = '%032x' % random.getrandbits(128)
-        if to_user == 0:
-            self.transactions[transaction_hash] = {
-                'amount': (self.want_to_pay[from_user] + len(self.user_ids) - 1) // len(self.user_ids),
-                'from': from_user,
-                'to': [user_id for user_id in self.user_ids if user_id != from_user],
-                'mask': [0] * (len(self.user_ids) - 1)
-            }
-        else:
-            self.transactions[transaction_hash] = {'amount': self.want_to_pay[from_user],
+        count = len(to_user)
+        if count > 1:
+            self.transactions[transaction_hash] = {'amount': (how_much + count) // (count + 1),
                                                    'from': from_user,
-                                                   'to': [to_user],
-                                                   'mask': [0]}
-        self.want_to_pay[from_user] = -1;
-        return transaction_hash, self.transactions[transaction_hash]
+                                                   'to': to_user,
+                                                   'mask': [0] * count,
+                                                   'transaction_hash': transaction_hash, }
+        else:
+            self.transactions[transaction_hash] = {'amount': how_much,
+                                                   'from': from_user,
+                                                   'to': to_user,
+                                                   'mask': [0],
+                                                   'transaction_hash': transaction_hash, }
+        return self.transactions[transaction_hash]
 
     def accept_transaction(self, user_id, transaction_hash):
-        if transaction_hash not in self.transactions.keys():
-            return False
         pos = self.transactions[transaction_hash]['to'].index(user_id)
         assert pos != -1, 'Not in tra'
         self.transactions[transaction_hash]['mask'][pos] = 1
@@ -45,10 +34,9 @@ class DebtDeligator(object):
             return self.transactions.pop(transaction_hash)
 
     def decline_transaction(self, user_id, transaction_hash):
-        if transaction_hash not in self.transactions.keys():
-            return False
-        pos = self.transactions[transaction_hash]['to'].index(user_id)
-        assert pos != -1, 'Not in tra'
+
+        assert user_id == self.transactions[transaction_hash]['from'] or \
+               user_id in self.transactions[transaction_hash]['to'], 'Not in tra'
         return self.transactions.pop(transaction_hash)
 
     def transact(self, transaction):
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import time
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.delegate import (
+    per_chat_id, create_open, pave_event_space, include_callback_query_chat_id)
 from configparser import ConfigParser
 from DebtDeligator import DebtDeligator
 
@@ -11,88 +13,192 @@ TOKEN = config.get('Secret Key', 'token')
 
 id_to_nick = {int(config['Chat Ids'].get(nick_name)): nick_name.capitalize() for nick_name in config['Chat Ids'].keys()}
 nick_to_id = {nick_name.capitalize(): int(config['Chat Ids'].get(nick_name)) for nick_name in config['Chat Ids'].keys()}
-deligator = DebtDeligator(list(id_to_nick.keys()))
+user_ids = list(id_to_nick.keys())
+deligator = DebtDeligator(user_ids)
+propose_records = telepot.helper.SafeDict()
+transaction_msgs = telepot.helper.SafeDict()
+transaction_hashes = telepot.helper.SafeDict()
 
 
 def get_debt(amount, user_name):
     if amount < 0:
-        return '{} to {}'.format(abs(amount), user_name)
+        return '{} -> {}'.format(abs(amount), user_name)
     else:
-        return '{} from {}'.format(abs(amount), user_name)
+        return '{} -> {}'.format(user_name, abs(amount))
 
 
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type != 'text' or chat_id not in id_to_nick.keys():
-        return
-    print(id_to_nick[chat_id], msg['text'])
-    if msg['text'].isdigit():
-        how_much = int(msg['text'])
-        if how_much > 0:
-            bot.sendMessage(chat_id, 'For who you paid {}?'.format(how_much),
-                            reply_markup=ReplyKeyboardMarkup(
-                                keyboard=[[KeyboardButton(text=id_to_nick[user_id]) for user_id in
-                                           id_to_nick.keys()
-                                           if msg['chat']['id'] != user_id] + [KeyboardButton(text='For all')]],
-                                one_time_keyboard=True))
-            deligator.set_amount(chat_id, how_much)
+class DebtBot(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(DebtBot, self).__init__(*args, **kwargs)
+
+        # Retrieve from database
+        if self.id in propose_records:
+            self._amount, self._to_ids, self._decline_msg_id = propose_records[self.id]
+            self._transaction_hash = transaction_hashes[self.id]
         else:
-            debt = deligator.get_debt(chat_id)
-            bot.sendMessage(chat_id,
-                            '\n'.join([get_debt(debt[chat_id], id_to_nick[chat_id]) for chat_id in debt]))
-    else:
-        if not deligator.is_amount_set(chat_id):
-            bot.sendMessage(chat_id, 'How much do you want to pay?')
-            return
-        if msg['text'] == 'For all':
-            transaction_hash, transaction = deligator.add_transaction(chat_id, 0)
-        elif msg['text'] in nick_to_id.keys() and msg['text'] != id_to_nick[chat_id]:
-            transaction_hash, transaction = deligator.add_transaction(chat_id, nick_to_id[msg['text']])
+            self._amount = None
+            self._to_ids = None
+            self._transaction_hash = None
+            self._decline_msg_id = None
+            transaction_hashes[self.id] = None
+
+    def _request(self):
+        if self._amount is None:
+            self._request_amount()
+        elif self._to_ids is None:
+            self._request_recipient()
         else:
-            return
+            print('All defined')
 
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text='Yes', callback_data='y:{}'.format(transaction_hash)),
-             InlineKeyboardButton(text='No', callback_data='n:{}'.format(transaction_hash))],
-        ])
+    def _request_amount(self):
+        if self._to_ids is None:
+            self.sender.sendMessage('Сколько ты хочешь заплатить?')
+        else:
+            self.sender.sendMessage(
+                'Сколько ты хочешь заплатить за {}?'.format(
+                    id_to_nick[self._to_ids[0]] if len(self._to_ids) == 1 else 'всех'))
 
-        for user_id in transaction['to']:
-            bot.sendMessage(user_id,
-                            'Accept payment {} from {}?'.format(transaction['amount'], id_to_nick[transaction['from']]),
-                            reply_markup=keyboard)
+    def _request_recipient(self):
+        msg = 'За кого ты хочешь заплатить?' if self._amount is None \
+            else 'За кого ты хочешь заплатить {}?'.format(self._amount)
 
+        self.sender.sendMessage(msg,
+                                reply_markup=ReplyKeyboardMarkup(
+                                    keyboard=[[KeyboardButton(text=id_to_nick[user_id]) for user_id in
+                                               user_ids
+                                               if user_id != self.id] + [
+                                                  KeyboardButton(text='За всех')]],
+                                    one_time_keyboard=True))
 
-def on_callback_query(msg):
-    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    print(id_to_nick[from_id], query_data)
-    if query_data[0] == 'y':
-        transaction = deligator.accept_transaction(from_id, query_data[2:])
-        if transaction == False:
-            bot.answerCallbackQuery(query_id, text='Already accepted or canceled')
-            return
-        bot.answerCallbackQuery(query_id, text='You accept it!')
-        if transaction is not None:
-            bot.sendMessage(transaction['from'],
-                            'We accept your payment: {}'.format(transaction['amount']))
+    def _sorry(self):
+        self.sender.sendMessage('Я не понимаю')
+
+    def _start_transaction(self):
+        self._transaction_hash = transaction_hashes[self.id]
+        if self._transaction_hash is None:
+            transaction = deligator.add_transaction(self.id, self._to_ids, self._amount)
+            self._transaction_hash = transaction['transaction_hash']
+            transaction_hashes[self.id] = transaction['transaction_hash']
+            print('new transaction:{}'.format(transaction['transaction_hash']))
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text='Да', callback_data='y:{}'.format(transaction['transaction_hash'])),
+                 InlineKeyboardButton(text='Нет', callback_data='n:{}'.format(transaction['transaction_hash']))],
+            ])
+
+            transaction_msgs[transaction['transaction_hash']] = {}
             for user_id in transaction['to']:
-                bot.sendMessage(user_id,
-                                'We add {} to your dept to {}'.format(transaction['amount'],
-                                                                      id_to_nick[transaction['from']]))
-    else:
-        transaction = deligator.decline_transaction(from_id, query_data[2:])
-        if transaction == False:
-            bot.answerCallbackQuery(query_id, text='Already accepted or canceled')
+                transaction_msgs[transaction['transaction_hash']][user_id] = \
+                    telepot.message_identifier(bot.sendMessage(user_id,
+                                                               'Принять платеж {} от {}?'.format(
+                                                                   transaction['amount'],
+                                                                   id_to_nick[transaction['from']]),
+                                                               reply_markup=keyboard))
+            self._amount = None
+            self._to_ids = None
+            self._transaction_hash = None
+        else:
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text='Да', callback_data='Y:{}'.format(self._transaction_hash)),
+                 InlineKeyboardButton(text='Нет', callback_data='N:{}'.format(self._transaction_hash))],
+            ])
+            self._decline_msg_id = telepot.message_identifier(
+                self.sender.sendMessage('У вас уже есть текущий запрос, вы хотите его отменить?',
+                                        reply_markup=keyboard))
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type != 'text':
+            print('Not text')
             return
-        bot.sendMessage(transaction['from'],
-                        'Sorry, {} decline payment from you: {}'.format(id_to_nick[from_id],
-                                                                        transaction['amount']))
-        bot.answerCallbackQuery(query_id, text='You decline it!')
+        print(id_to_nick[chat_id], msg['text'])
+
+        if msg['text'].isdigit():
+            amount = int(msg['text'])
+            if amount > 0:
+                self._amount = amount
+            else:
+                debt = deligator.get_debt(chat_id)
+                self.sender.sendMessage(
+                    '\n'.join([get_debt(debt[chat_id], id_to_nick[chat_id]) for chat_id in debt]))
+                return
+        else:
+            if msg['text'] == 'За всех':
+                self._to_ids = [user_id for user_id in user_ids if user_id != chat_id]
+            elif msg['text'] in nick_to_id.keys() and msg['text'] != id_to_nick[chat_id]:
+                self._to_ids = [nick_to_id[msg['text']]]
+            else:
+                self._sorry()
+
+        if self._amount is None or self._to_ids is None:
+            self._request()
+        else:
+            self._start_transaction()
+
+    def _close_transaction(self, transaction, result):
+        transaction_hash = transaction['transaction_hash']
+        if result:
+            bot.sendMessage(transaction['from'], 'Ваша транакция подтверждена')
+            transaction_hashes[transaction['from']] = None
+            for user_id in transaction['to']:
+                bot.editMessageText(transaction_msgs[transaction_hash][user_id],
+                                    'Оплата успешно подтверждена\n{} заплатил за вас {}'.format(
+                                        id_to_nick[transaction['from']], transaction['amount']), reply_markup=None)
+                bot.sendMessage(transaction['from'],
+                                'Вы заплатили {1} за {0}'.format(id_to_nick[user_id], transaction['amount']))
+            del transaction_msgs[transaction_hash]
+        else:
+            bot.sendMessage(transaction['from'], 'Вашу транакцию отменили')
+            transaction_hashes[transaction['from']] = None
+            for user_id in transaction['to']:
+                msg = 'Вы отменили оплату' if user_id == self.id else '{} отменил оплату'.format(id_to_nick[self.id])
+                bot.editMessageText(transaction_msgs[transaction_hash][user_id],
+                                    msg, reply_markup=None)
+            del transaction_msgs[transaction_hash]
+
+    def on_callback_query(self, msg):
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+        print(id_to_nick[from_id], query_data)
+        transaction_hash = query_data[2:]
+        if query_data[0] == 'y':
+            transaction = deligator.accept_transaction(from_id, transaction_hash)
+            bot.answerCallbackQuery(query_id, text='You accept it!')
+            bot.editMessageText(transaction_msgs[transaction_hash][self.id],
+                                'Ожидаем подтверждения от других получателей', reply_markup=None)
+            if transaction is not None:
+                self._close_transaction(transaction, True)
+        elif query_data[0] == 'n':
+            transaction = deligator.decline_transaction(from_id, query_data[2:])
+            bot.answerCallbackQuery(query_id, text='You decline it!')
+            bot.editMessageText(transaction_msgs[transaction_hash][self.id],
+                                'Ожидаем подтверждения от других получателей', reply_markup=None)
+            self._close_transaction(transaction, False)
+        elif query_data[0] == 'Y':
+            transaction = deligator.decline_transaction(from_id, query_data[2:])
+            bot.answerCallbackQuery(query_id, text='You decline it!')
+            bot.editMessageText(self._decline_msg_id, 'Вы отменили оплату', reply_markup=None)
+            self._close_transaction(transaction, False)
+            self._start_transaction()
+            pass
+        elif query_data[0] == 'N':
+            bot.answerCallbackQuery(query_id, text='Ok, waiting!')
+            bot.editMessageText(self._decline_msg_id, 'Подождем', reply_markup=None)
+        else:
+            pass
+
+    def on__idle(self, event):
+        self.close()
+
+    def on_close(self, ex):
+        # Save to database
+        propose_records[self.id] = (self._amount, self._to_ids, self._decline_msg_id)
 
 
-bot = telepot.Bot(TOKEN)
-
-MessageLoop(bot, {'chat': on_chat_message,
-                  'callback_query': on_callback_query}).run_as_thread()
+bot = telepot.DelegatorBot(TOKEN, [
+    include_callback_query_chat_id(
+        pave_event_space())(
+        per_chat_id(types=['private']), create_open, DebtBot, timeout=10),
+])
+MessageLoop(bot).run_as_thread()
 print('Listening ...')
 
 while 1:
